42dcf2c73f258a641bef9f74806ebee2273d716b
fierysleo <jolexandersan@gmail.com>
2025-10-18 17:39:25 +0800

    satisfactory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
+
+//Home
 const container = document.querySelector("#content");
 const h1 = document.createElement("h1");
-div.textContent = "Welcome to Something Resturant";
-const h3 = document.createElement("h3");
-h3.textContent = "We serve the best something using the something recipe served by our best something Resturant chef";
-container.append(h1, h3);
+h1.textContent = "Welcome to Something Resturant";
+
+//Menu
+const img1 = document.createElement("img");
+img1.src = "assets/something1.jpg";
+img1.alt = "Something Dish";
+const img2 = document.createElement("img");
+img2.src = "assets/something2.jpg";
+img2.alt = "Something Dish";
+const img3 = document.createElement("img");
+img3.src = "assets/something3.jpg";
+img3.alt = "Something Dish";
+
+const btnHome = document.querySelector("#BtnHome");
+btnHome.addEventListener("click", () => {
+    container.replaceChildren(h1);
+});
+
+const btnMenu = document.querySelector("#BtnMenu");
+btnMenu.addEventListener("click", () => {
+    container.replaceChildren(img1, img2, img3);
+});
+
+
+
