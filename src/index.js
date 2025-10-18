58d085f4c24790574549dabdceba5044cc72e867
fierysleo <jolexandersan@gmail.com>
2025-10-18 17:01:27 +0800

    DOM initiated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+const container = document.querySelector("#content");
+const h1 = document.createElement("h1");
+div.textContent = "Welcome to Something Resturant";
+const h3 = document.createElement("h3");
+h3.textContent = "We serve the best something using the something recipe served by our best something Resturant chef";
+container.append(h1, h3);
